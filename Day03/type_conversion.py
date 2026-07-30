0aa8142cce24469f2af10cd091d0258deb5cf93d
Venkatramana0229 <kvenkatramana0229@gmail.com>
2026-07-30 06:11:57 +0530

    Create type_conversion.py

---
+++ Day03/type_conversion.py
@@ -0,0 +1,15 @@
+x = input("Enter any number= ")
+
+print(f"{x} is {type(x)}")
+
+x = float(x)
+print(f"{x} is {type(x)}")
+
+x = str(x)
+print(f"{x} is {type(x)}")
+
+x = bool(x)
+print(f"{x} is {type(x)}")
+
+x = int(x)
+print(f"{x} is {type(x)}")

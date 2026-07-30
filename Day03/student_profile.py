acaeb35a7a1a4351ccebf28bc64d0261fdbe4dc8
Venkatramana0229 <kvenkatramana0229@gmail.com>
2026-07-30 06:20:07 +0530

    Create student_profile.py

---
+++ Day03/student_profile.py
@@ -0,0 +1,11 @@
+age = 18
+name = "K.venkat Ramana"
+branch = "CSE-(Ai&ml)"
+college = "JNTU wanaparthy"
+cgpa = 8.5
+
+print("------Student profile------")
+print("Name : ",name)
+print("Age : ",age)
+print("Branch : ",branch)
+print("College : ",college)

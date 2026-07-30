4804b9f535f13f43b76aa8d2058338c73502e547
Venkatramana0229 <kvenkatramana0229@gmail.com>
2026-07-30 06:01:40 +0530

    Create data_types.py

---
+++ Day03/data_types.py
@@ -0,0 +1,9 @@
+name = input("Enter name:")
+age = int(input("Enter age:"))
+height = float(input("Enter height"))
+is_student = True
+
+print(f"{name} is {type(name)}")
+print(f"{age} is {type(age)}")
+print(f"{height} is {type(height)}")
+print(f"{is_student} is {type(is_student)}")

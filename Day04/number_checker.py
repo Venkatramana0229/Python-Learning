924c7177713445849be61d1b42440b0ead64db2b
Venkatramana0229 <kvenkatramana0229@gmail.com>
2026-08-02 21:06:17 +0530

    Create number_checker.py

---
+++ Day04/number_checker.py
@@ -0,0 +1,13 @@
+n = int(input("Enter any number= "))
+
+if(n>0):
+    print("Given number is a positive integer")
+elif(n<0):
+    print("Given number is a negative integer")
+else:
+    print("Given number is zero")
+
+if(n%2==0):
+    print("Given number is even")
+else:
+    print("Given number is odd") 
